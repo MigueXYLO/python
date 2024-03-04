a329010c730f5b91d9ae015afaa8ba08c37bc31d
migueXYLO <2221462@my.ipleiria.pt>
2024-03-04 12:14:05 +0000

    I guess???

---
+++ 02_neuron/main.py
@@ -0,0 +1,24 @@
+from percepron import Perceptron  # Importing the Perceptron class from the perceptron module
+import numpy as np  # Importing the numpy library and aliasing it as np
+
+if __name__ == '__main__':
+    # Defining the training data as a numpy array
+    training_data = np.array([[-1, -1], [-1, 1], [1, -1], [1, 1]])
+    
+    # Defining the corresponding training labels as a numpy array
+    training_label = np.array([-1, -1, -1, 1])
+
+    # Creating an instance of the Perceptron class
+    perceptron = Perceptron()
+
+    # Training the perceptron using the fit() method
+    # The learning_rate parameter is set to 0.1 and the seed parameter is set to 1337
+    perceptron.fit(training_data, training_label, learning_rate=0.1, seed=1337)
+
+    # Looping through each sample in the training data
+    for sample in training_data:
+        # Predicting the output for each sample using the predict() method
+        output = perceptron.predict(sample)
+        
+        # Printing the sample and its corresponding output
+        print(str(sample) + ' ' + str(output))
